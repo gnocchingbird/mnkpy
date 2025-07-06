a5816025de5c44daa3ed514ee30c2ea6d74495e1
gnoc <lflaack@protonmail.com>
2025-07-06 19:56:19 +0200

    Initialize

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# (n, m, k)-game implemented in python
+# on a n by m board, two players take turns placing pieces.
+# the game ends either when one player has placed k pieces vertically, horizontally or diagonally, or when the board is filled without a winner.
+
+class Game(object):
+    def __init__(self, n: int, m: int, k: int):
+        self.board = np.zeros((n, m), dtype = np.byte)
+        self.k = k
+
+    def __repr__(self):
+        return self.board.__repr__()
+    
+    def is_full(self):
+        shape = self.board.shape
+        return len(np.flatnonzero(self.board)) == shape[0] * shape[1] # as many nonzero fields as existing fields
+
+    
+
+if __name__ == "__main__":
+    game = Game(1, 1, 3)
+    game.board[0, 0] = 1
+    print(game.is_full())
+    print(game)
