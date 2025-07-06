c65e467ce2ce0ee641c14d31d179627e705f9a8f
gnoc <lflaack@protonmail.com>
2025-07-07 00:47:58 +0200

    Implement slicing for victory checking

--- main.py
+++ main.py
@@ -1,22 +1,121 @@
 import numpy as np
-
+import time
+from datetime import timedelta
 
 class Game(object):
-    def __init__(self, n: int, m: int, k: int):
-        self.board = np.zeros((n, m), dtype = np.byte)
+    def __init__(self, m: int, n: int, k: int):
+        self.board = np.zeros((m, n), dtype = np.byte)
         self.k = k
+        self.slices = [] # contains lists of lists of coordinate pairs corresponding to the board sliced vertically, horizontally and diagonally (twice).
+        self.slice(m, n, k)
 
     def __repr__(self):
         return self.board.__repr__()
     
-    def is_full(self):
+    def is_full(self) -> bool:
         shape = self.board.shape
         return len(np.flatnonzero(self.board)) == shape[0] * shape[1] # as many nonzero fields as existing fields
+
+    def is_won(self) -> bool:
+        return False
+
+    def place(self, x: int, y: int, player: int) -> bool:
+        if self.board[x, y] != 0:
+            raise(f"Illegal move! ({x},{y}) is already occupied by player {self.board[x, y]}!")
+            return False
+        else:
+            self.board[x, y] = player
+
+    def slice(self, m: int, n: int, k: int) -> None:
+        slices = []
+
+        v = []
+        if n >= k:
+            for y in range(m):
+                col = []
+                for x in range(n):
+                    col.append((x, y))
+                v.append(col)
+        
+        h = []
+        if m >= k:
+            for x in range(n):
+                row = []
+                for y in range(m):
+                    row.append((x, y))
+                h.append(row)
+        
+        d1 = [] # top left to bottom right
+        #if m >= k and n >= k:
+        #    for i in range(-(m - k), 0):
+        if m >= k and n >= k:
+            x, y = 0, m - k
+            while y > 0:
+                x_, y_ = x, y
+                dia = []
+                while x_ < n and y_ < m:
+                    dia.append((x_, y_))
+                    x_ += 1
+                    y_ += 1
+                d1.append(dia)
+                y -= 1
+            while x <= n - k:
+                x_, y_ = x, y
+                dia = []
+                while x_ < n and y_ < m:
+                    dia.append((x_, y_))
+                    x_ += 1
+                    y_ += 1
+                d1.append(dia)
+                x += 1
+
+        d2 = [] # bottom left to top right
+        if m >= k and n >= k:
+            x, y = 0, k - 1
+            while y < m - 1:
+                x_, y_ = x, y
+                dia = []
+                while x_ < n and y_ >= 0:
+                    dia.append((x_, y_))
+                    x_ += 1
+                    y_ -= 1
+                d2.append(dia)
+                y += 1
+            while x <= n - k:
+                x_, y_ = x, y
+                dia = []
+                while x_ < n and y_ >= 0:
+                    dia.append((x_, y_))
+                    x_ += 1
+                    y_ -= 1
+                d2.append(dia)
+                x += 1
+        
+        self.slices.extend(v)
+        self.slices.extend(h)
+        self.slices.extend(d1)
+        self.slices.extend(d2)
+
+    
+
 
     
 
 if __name__ == "__main__":
-    game = Game(1, 1, 3)
-    game.board[0, 0] = 1
+    start_time = time.monotonic()
+    game = Game(4, 5, 3)
+    game.board = np.arange(20).reshape((4, 5))
+    game.board[1, 2] = 1
     print(game.is_full())
     print(game)
+    print(game.slices)
+    for slice in game.slices:
+        x, y = list(zip(*slice))
+        print(game.board[[y, x]])
+
+
+
+
+
+
+    print(timedelta(seconds=time.monotonic() - start_time))
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-## nmkpy
+## mnkpy
 
 ### rules:
 
-- (n, m, k)-game implemented in python
-- on a n by m board, two players take turns placing pieces.
+- (m, n, k)-game implemented in python
+- on a m by n board, two players take turns placing pieces.
 - the game ends either when one player has placed k pieces vertically, horizontally or diagonally, or when the board is filled without a winner.
